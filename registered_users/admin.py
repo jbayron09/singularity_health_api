2ffe4d0a7816ef1010b1559fd5f6a6830eb677a8
jbayron09 <j.bayron.b@gmail.com>
2025-05-12 09:21:33 -0500

    configure Django admin for registered_users models
    - Registered AppUser, UserDocument, ContactInfo, TypeDocument, and Country models
    - Customized admin views with list displays, search fields, and readonly fields
    - Ensured important fields are properly organized and secured in admin forms

--- registered_users/admin.py
+++ registered_users/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from registered_users.models import (
+    AppUser,
+    UserDocument,
+    TypeDocument,
+    ContactInfo,
+    Country
+)
 
-# Register your models here.
+
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "name", "last_name", "is_militar", "is_temporal", "time_create")
+    search_fields = ("username", "email", "name", "last_name")
+    readonly_fields = ("time_create",)
+    fields = (
+        "username", "email", "name", "last_name",
+        "password", "is_militar", "is_temporal",
+        "email_verified", "verification_token", "time_create"
+    )
+
+
+@admin.register(UserDocument)
+class UserDocumentAdmin(admin.ModelAdmin):
+    list_display = ("user", "document", "type_document", "place_expedition", "date_expedition")
+    search_fields = ("document", "user__username", "user__email")
+    fields = (
+        "user", "type_document", "document",
+        "place_expedition", "date_expedition"
+    )
+
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = ("user", "city", "phone", "cel_phone", "country")
+    search_fields = ("user__username", "city", "cel_phone")
+    fields = (
+        "user", "address", "city", "phone", "cel_phone",
+        "emergency_name", "emergency_phone", "country"
+    )
+
+
+@admin.register(TypeDocument)
+class TypeDocumentAdmin(admin.ModelAdmin):
+    list_display = ("id", "name_type_document",)
+    search_fields = ("name_type_document",)
+    fields = ("name_type_document",)
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("country_code", "country_name",)
+    search_fields = ("country_code", "country_name")
+    fields = ("country_code", "country_name",)
